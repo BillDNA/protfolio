bc830f2aec6acee1718f22cbc98ea2845178131c
Bill Decker <bill.s.decker@gmail.com>
2018-02-24 10:09:19 -0800

    adding nav and some css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+//css
+import '../css/Nav.css'
+
 class Nav  extends React.Component {
 	render() {
 		return (
 			<div className="nav-bar">
-				Nav Bar
+				<div className="nav-bar-location">
+					<div className="nav-bar-title">
+						<p><span>D</span>ynamic</p>
+						<p><span>N</span>ew</p>
+						<p><span>A</span>lgorithms</p>
+					</div>
+					<div className="nav-bar-sub-line">{this.props.location}</div>
+
+				</div>
 			</div>
 		);
 	}
